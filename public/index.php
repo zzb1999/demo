de9acd054d9c4ea35c111d05a9ddae882dba8f14
ixysec <1477821088@qq.com>
2020-05-29 17:36:37 +0800

    test

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+use zzb1999\demo\base\User;
+
+# 引入自动加载处理
+require "../vendor/autoload.php";
+
+echo User::show();
+echo app();
---
+++ src/base/User.php
@@ -0,0 +1,8 @@
+<?php
+namespace zzb1999\demo\base;
+
+class User{
+	public static function show(){
+		return "laot NB";
+	}
+}
